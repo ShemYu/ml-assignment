da27aaad3bddc70c3a6cd1a6352af759ffeb71e7
ShemYu <shauns4y@gmail.com>
2023-07-26 23:11:30 +0800

    Udpate IO data model's example

--- app/models/translation.py
+++ app/models/translation.py
@@ -1,22 +1,36 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Record(BaseModel):
-    id: str
-    text: str
+    id: str = Field(...)
+    text: str = Field(...)
 
 
-class Payload(BaseModel):
-    fromLang: str
-    records: List[Record]
-    toLang: str
+class TranslationPayload(BaseModel):
+    fromLang: str = Field(...)
+    toLang: str = Field(...)
+    records: List[Record] = Field(...)
 
 
 class TranslationRequest(BaseModel):
-    payload: Payload
+    payload: TranslationPayload = Field(...)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "payload": {
+                    "fromLang": "en",
+                    "records": [{"id": "123", "text": "Life is like a box of chocolates."}],
+                    "toLang": "ja",
+                }
+            }
+        }
 
 
 class TranslationResponse(BaseModel):
     result: List[Record]
+
+    class Config:
+        json_schema_extra = {"example": {"result": [{"id": "123", "text": "人生はチョコレートの箱のようなものだ。"}],}}
