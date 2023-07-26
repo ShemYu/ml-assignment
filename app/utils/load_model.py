4ca95d67798ac11d7ed0bf2241261a0d5f1f41a1
ShemYu <shauns4y@gmail.com>
2023-07-26 22:42:19 +0800

    Add startup event for model loading

--- app/utils/load_model.py
+++ app/utils/load_model.py
@@ -1,9 +1,16 @@
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 
 
-def load_model():
-    model = M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100_418M")
-    tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_418M", src_lang="en", tgt_lang="fr")
+model = None
+tokenizer = None
+
+
+async def load_model():
+    global model, tokenizer
+    if model == None:
+        model = M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100_418M")
+    if tokenizer == None:
+        tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_418M", src_lang="en", tgt_lang="fr")
     return model, tokenizer
 
 
