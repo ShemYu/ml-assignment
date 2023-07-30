924e07a697b5797355b3d1ce4e88a6c0a013017e
Shem Yu <shauns4y@gmail.com>
2023-07-30 07:10:25 +0000

    Update deployment shellcript and yaml

--- bin/minikube_build_and_deploy.sh
+++ bin/minikube_build_and_deploy.sh
@@ -2,7 +2,10 @@ eval $(minikube -p minikube docker-env)
 DOCKER_TAG=translate_service:latest
 DOCKER_BUILD_CONTEXT=.
 DOCKER_FILE_PATH=Dockerfile
-MINIKUBE_REGISTRY=10.102.32.249:80
+REGISTRY_CLUSTER_IP=$(kubectl get svc registry -n kube-system -o=jsonpath='{.spec.clusterIP}')
+export MINIKUBE_REGISTRY
 docker build -t $DOCKER_TAG -f $DOCKER_FILE_PATH $DOCKER_BUILD_CONTEXT
-docker tag $DOCKER_TAG $MINIKUBE_REGISTRY/$DOCKER_TAG
-docker push $MINIKUBE_REGISTRY/$DOCKER_TAG
+docker tag $DOCKER_TAG $REGISTRY_CLUSTER_IP:80/$DOCKER_TAG
+docker push $REGISTRY_CLUSTER_IP:80/$DOCKER_TAG
+envsubst < k8s/deployment.yaml | kubectl apply -f -
+kubectl port-forward svc/translation-service 9527:9527 # Only for testing on local
