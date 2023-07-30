739fa7ee849e3c5fc78e667dec20eed9bba81e92
ShemYu <shauns4y@gmail.com>
2023-07-30 17:13:13 +0800

    Writing docstring of load model function

--- app/utils/load_model.py
+++ app/utils/load_model.py
@@ -1,17 +1,24 @@
 import torch
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 
-
 model = None
 tokenizer = None
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 async def load_model():
+    """
+    Asynchronously loads the M2M100 model and tokenizer from the pretrained
+    `facebook/m2m100_418M` package if not already loaded.
+
+    Returns:
+        model (M2M100ForConditionalGeneration): The loaded M2M100 model.
+        tokenizer (M2M100Tokenizer): The loaded M2M100 tokenizer.
+        device (torch.device): The device (either "cuda" if available, or "cpu") where the model will be run.
+    """
     global model, tokenizer
-    if model == None:
+    if model is None:
         model = M2M100ForConditionalGeneration.from_pretrained("facebook/m2m100_418M")
         model.to(device)
-    if tokenizer == None:
+    if tokenizer is None:
         tokenizer = M2M100Tokenizer.from_pretrained("facebook/m2m100_418M")
     return model, tokenizer, device
