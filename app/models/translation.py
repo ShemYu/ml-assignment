1bdf6c740ebf53371482dd5681bd2ca696d65bb0
ShemYu <shauns4y@gmail.com>
2023-07-30 16:55:52 +0800

    Update docstring of data model

--- app/models/translation.py
+++ app/models/translation.py
@@ -4,18 +4,18 @@ from pydantic import BaseModel, Field
 
 
 class Record(BaseModel):
-    id: str = Field(...)
-    text: str = Field(...)
+    id: str = Field(..., description="Unique identifier for each record")
+    text: str = Field(..., description="The text to be translated")
 
 
 class TranslationPayload(BaseModel):
-    fromLang: str = Field(...)
-    toLang: str = Field(...)
-    records: List[Record] = Field(...)
+    fromLang: str = Field(..., description="Source language code (e.g., 'en' for English)")
+    toLang: str = Field(..., description="Target language code (e.g., 'ja' for Japanese)")
+    records: List[Record] = Field(..., description="List of records to be translated")
 
 
 class TranslationRequest(BaseModel):
-    payload: TranslationPayload = Field(...)
+    payload: TranslationPayload = Field(..., description="Payload containing all necessary details for the translation request")
 
     class Config:
         json_schema_extra = {
@@ -30,11 +30,7 @@ class TranslationRequest(BaseModel):
 
 
 class TranslationResponse(BaseModel):
-    result: List[Record]
+    result: List[Record] = Field(..., description="List of records containing the translation results")
 
     class Config:
-        json_schema_extra = {
-            "example": {
-                "result": [{"id": "123", "text": "人生はチョコレートの箱のようなものだ。"}],
-            }
-        }
+        json_schema_extra = {"example": {"result": [{"id": "123", "text": "人生はチョコレートの箱のようなものだ。"}],}}
