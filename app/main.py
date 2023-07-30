c2c6b39b11f4250a670bee2f8d94c8d4e9562d6c
Shem Yu <shauns4y@gmail.com>
2023-07-30 07:21:10 +0000

    Writing docstring

--- app/main.py
+++ app/main.py
@@ -11,9 +11,24 @@ exception_handler.load_exception_handler(app)
 
 @app.on_event("startup")
 async def startup_event():
+    """Startup event to load the translation model.
+
+    This function loads the translation model when the application starts.
+    
+    Returns:
+        None
+    """
     _ = await load_model.load_model()
 
 
 @app.get("/healthz")
 async def health_check():
+    """Health check endpoint.
+
+    This endpoint is used to check the health of the application.
+    A successful response indicates that the application is running as expected.
+
+    Returns:
+        dict: A JSON object with a single key `status` with a value of "OK".
+    """
     return {"status": "OK"}
